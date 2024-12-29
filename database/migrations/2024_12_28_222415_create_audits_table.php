41849fb3d829d0a66944b869b5d29e4c4d210a55
Nemanja Janicijevic <nem.janicijevic95@gmail.com>
2024-12-29 19:05:51 +0100

    Migration final

---
+++ database/migrations/2024_12_28_222415_create_audits_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('audits', function (Blueprint $table) {
+            $table->id();
+            $table->integer('row');
+            $table->string('column');
+            $table->foreignId('upload_id')->constrained();
+            $table->string('old_value');
+            $table->string('new_value');
+            $table->string('model');
+            $table->string('model_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('audits');
+    }
+};
